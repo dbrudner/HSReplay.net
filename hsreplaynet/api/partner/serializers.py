3369276d7cc465ded84efc55d3709edde1fbd75e
Benedict Etzel <benedict@hearthsim.net>
2018-08-06 18:51:51 +0200

    Include 5 archetypes in the classes Partner API

--- hsreplaynet/api/partner/serializers.py
+++ hsreplaynet/api/partner/serializers.py
@@ -364,7 +364,7 @@ class ClassArchetypeSummarySerializer(Serializer):
 		result = []
 		archetypes = sorted(instance.values(), key=sort_fn, reverse=True)
 
-		for archetype in archetypes[0:3]:
+		for archetype in archetypes[0:5]:
 			serializer = ClassArchetypeStatsSerializer(archetype)
 			result.append(serializer.data)
 
